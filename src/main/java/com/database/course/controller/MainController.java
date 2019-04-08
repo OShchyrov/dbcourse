6db65eba2f1ebb7a89785f74c7750963995c8fa6
alexander <olexandr.shchirov@gmail.com>
2019-04-09 01:29:31 +0300

    init commit

--- src/main/java/com/database/course/controller/MainController.java
+++ src/main/java/com/database/course/controller/MainController.java
@@ -2,6 +2,10 @@ package com.database.course.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class MainController {
@@ -9,5 +13,18 @@ public class MainController {
     @RequestMapping(name = "/")
     public String index() {
         return "main";
+    }
+
+    @RequestMapping(name = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(name = "/login", method = RequestMethod.POST)
+    public String login(Map<String, Object> model,
+                        @RequestParam String login,
+                        @RequestParam String password) {
+
+        return "login";
     }
 }

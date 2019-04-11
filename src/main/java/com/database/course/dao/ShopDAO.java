87b36fc67cebfbf0bd4a869e8688b99714ce585b
alexander <olexandr.shchirov@gmail.com>
2019-04-12 00:49:34 +0300

    Improved updating and removing according to the new tables

--- src/main/java/com/database/course/dao/ShopDAO.java
+++ src/main/java/com/database/course/dao/ShopDAO.java
@@ -40,16 +40,23 @@ public class ShopDAO {
 
     public void update(String oldShopname, String oldCity, String shopname, String category, String city, String latitude, String longitude, String manager) {
         template.update("UPDATE shops s INNER JOIN shops_categories sc ON s.shopname = sc.shopname " +
-                "INNER JOIN product_shop ps ON s.shopname = ps.shop_name " +
-                "SET s.shopname = ?, sc.shopname = ?, ps.shop_name = ?, " +
-                "sc.category = ?, s.city = ?, s.latitude = ?, s.longitude = ?, s.manager = ? " +
+                        "INNER JOIN product_shop ps ON s.shopname = ps.shop_name " +
+                        "INNER JOIN product_score psc ON psc.shopname = s.shopname " +
+                        "INNER JOIN products_promo pp ON pp.shopname = s.shopname " +
+                        "INNER JOIN shop_manager sm ON sm.shopname = s.shopname " +
+                        "SET s.shopname = ?, sc.shopname = ?, ps.shop_name = ?, psc.shopname = ?, pp.shopname = ?, sm.shopname = ?, " +
+                        "sc.category = ?, s.city = ?, s.latitude = ?, s.longitude = ?, s.manager = ?, sm.manager = ? " +
                         "WHERE s.shopname = ? AND s.city = ?",
-                shopname, shopname, shopname, category, city, latitude, longitude, manager, oldShopname, oldCity);
+                shopname, shopname, shopname, shopname, shopname, shopname,
+                category, city, latitude, longitude, manager, manager,
+                oldShopname, oldCity);
     }
 
     public void remove(String shopname, String city) {
         template.update("DELETE FROM shops WHERE shopname = ? AND city = ?", shopname, city);
         template.update("DELETE FROM shops_categories WHERE shopname = ?", shopname);
         template.update("DELETE FROM product_shop WHERE shop_name = ?", shopname);
+        template.update("DELETE FROM product_score WHERE shopname = ?", shopname);
+        template.update("DELETE FROM shop_manager WHERE shopname = ?", shopname);
     }
 }
